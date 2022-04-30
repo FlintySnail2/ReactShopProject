560caf439765c161575f2d70e54bdf46526a1029
Harry <Hazmclean92@gmail.com>
2022-04-30 21:07:14 +1000

    Added New Project

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+// react-navigation/native
+
+//IMPORT VIEWS
+import Home from './src/views/Home' 
+import  Category from './src/views/Category';
+import ProductDetail from './src/views/ProductDetail';
+
+var HomeStack=createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <NavigationContainer>
+      <HomeStack.Navigator>
+        <HomeStack.Screen name="Home" component={Home}/>
+        <HomeStack.Screen name="Category" component={Category}/>
+        <HomeStack.Screen name="ProductDetail" component={ProductDetail}/>
+
+      </HomeStack.Navigator>
+    </NavigationContainer>
+
+  
   );
 }
 
